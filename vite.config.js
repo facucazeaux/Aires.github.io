de9a20a20a00b1124a9e0f30af85d49e27c73404
facucazeaux <144554737+facucazeaux@users.noreply.github.com>
2026-04-17 20:04:46 -0300

    Corregir rutas de imagenes con punto relativo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Esto hace que todas las rutas sean relativas automáticamente
+  base: '/Aires.github.io/', // El nombre exacto de tu repositorio entre barras
 })
